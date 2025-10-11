84c6abad51753d24149347b799536f54909e22fc
Deepankar <deepankar7493@gmail.com>
2025-10-11 16:08:16 +0530

    50%

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const table = document.querySelector(".book-table");
 const tbody = document.createElement("tbody");
+const btn = document.querySelector(".btn");
+const dispForm = document.querySelector(".bform");
+const form = document.querySelector(".book");
 
 const myLibrary = [];
 function Book(title, author, pages, status, pending) {
@@ -14,7 +17,6 @@ function Book(title, author, pages, status, pending) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  console.log(myLibrary);
 }
 
 const book1 = new Book("Harry Potter", "J.k.", 543, true, "pen");
@@ -36,3 +38,39 @@ function createTable() {
   table.appendChild(tbody);
 }
 createTable();
+
+function appendNewBook() {
+  const tr = document.createElement("tr");
+  let entry = myLibrary[myLibrary.length - 1];
+  Object.values(entry).forEach((value) => {
+    const td = document.createElement("td");
+    td.textContent = value;
+    tr.appendChild(td);
+  });
+  tbody.appendChild(tr);
+  table.appendChild(tbody);
+}
+
+btn.addEventListener("click", () => {
+  dispForm.classList.remove("hidden");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+
+  const book = new Book(
+    data.Book,
+    data.Author,
+    Number(data.Pages),
+    data.status,
+    "pen"
+  );
+  addBookToLibrary(book);
+  appendNewBook();
+  console.log(myLibrary);
+  form.reset();
+  dispForm.classList.add("hidden");
+});
